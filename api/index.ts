9f869ead2db8f3d8aa462a13cb8d11514b3c2c6c
satyam priyanshu gupta <satyampriyanshugupta@satyams-MacBook-Air-3.local>
2026-09-15 17:18:35 +0530

    Fix typescript ESM import extension

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import Product from './models/Product';
+import Product from './models/Product.js';
 
 dotenv.config();
 
